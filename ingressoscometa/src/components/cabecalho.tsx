7ffe16b78b7c29b4342deb342bf0c73769b91eb6
lipealves13 <lipealves22@gmail.com>
2023-04-15 18:32:54 -0300

    NextJs create app - criação do projeto nextjs

---
+++ ingressoscometa/src/components/cabecalho.tsx
@@ -0,0 +1,7 @@
+const cabecalho = 
+    <div>
+        <h1>Início</h1>
+        <h2>Ingressos Cometa</h2>
+    </div>
+
+export default cabecalho
---
+++ ingressoscometa/src/pages/index.tsx
@@ -0,0 +1,6 @@
+import { cabecalho } from "../components/cabecalho.tsx"
+export default function home(){
+    return (
+        {cabecalho}
+    )
+}
