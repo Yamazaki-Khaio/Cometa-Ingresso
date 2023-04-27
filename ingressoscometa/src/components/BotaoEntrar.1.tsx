3982aea9ddafaf5ecd39b65ebcedfa1ae04b6d7d
Pedro Santos <psgbi123@gmail.com>
2023-04-27 16:00:45 -0300

    Botão de Entrar Adicionado

---
+++ ingressoscometa/src/components/BotaoEntrar.1.tsx
@@ -0,0 +1,11 @@
+export default function BotaoEntrar(){
+    return(<div>
+        <button action='onValidate'>
+            Entrar
+        </button>
+        <Script>
+            onValidate(){
+            }
+        </Script>
+        </div>);
+}
