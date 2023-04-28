3e155c6737ad56825a505ac2d9e85f284348facb
marcosviniciussena <marcvinisena@gmail.com>
2023-04-27 22:39:07 -0300

    Função login

--- ingressoscometa/src/pages/api/classes/Usuario.tsx
+++ ingressoscometa/src/pages/api/classes/Usuario.tsx
@@ -1,24 +1,42 @@
 class User {
-    private name: string;
-    private age: number;
-    private email: string;
-  
-    constructor(name: string, age: number, email: string) {
-      this.name = name;
-      this.age = age;
-      this.email = email;
-    }
-  
-    public getName(): string {
-      return this.name;
-    }
-  
-    public getAge(): number {
-      return this.age;
-    }
-  
-    public getEmail(): string {
-      return this.email;
-    }
+  private name: string;
+  private age: number;
+  private email: string;
+  private senha: string;
+
+  constructor(name: string, age: number, email: string, senha: string) {
+    this.name = name;
+    this.age = age;
+    this.email = email;
+    this.senha = senha;
   }
-  
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public getAge(): number {
+    return this.age;
+  }
+
+  public getEmail(): string {
+    return this.email;
+  }
+
+  public getSenha(): string{
+    return this.senha;
+  }
+
+  public login(loginAtual: string, senhaAtual: string){
+    var logado:boolean = false;
+
+    if (loginAtual == this.email){
+      if(senhaAtual == this.senha){
+        logado = true;
+      }
+    }
+
+    return logado;
+  }
+
+}
