b5416c62b6675d4b316d073cf529113e8f563e3c
Yamazaki Khaio <kaio_7fsa@hotmail.com>
2023-04-28 14:28:18 -0300

    init_db

---
+++ ingressoscometa/src/pages/api/dbConecct.tsx
@@ -0,0 +1,40 @@
+import express from "express";
+import mysql from "mysql2";
+
+const app = express();
+
+// Configuração do banco de dados
+const connection = mysql.createConnection({
+  host: "dbtecstorm-1.ciavjkfiolo8.us-east-2.rds.amazonaws.com",
+  user: "tecstorm",
+  password: "vaidormir",
+  database: "cometadb",
+});
+
+// Testando conexão com o banco de dados
+connection.connect((err) => {
+  if (err) {
+    console.error("Erro ao conectar ao banco de dados: ", err);
+    return;
+  }
+  console.log("Conexão com o banco de dados estabelecida!");
+});
+
+// Adicionando uma rota que retorna os dados do banco de dados
+app.get("/dados", (req, res) => {
+  const sql = "SELECT * FROM tabela";
+  connection.query(sql, (err, result) => {
+    if (err) {
+      console.error("Erro ao consultar dados: ", err);
+      res.status(500).send("Erro ao consultar dados.");
+      return;
+    }
+    res.json(result);
+  });
+});
+
+// Iniciando o servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
