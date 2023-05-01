e9fc28130faf1704360c2d33330b2dc4517bf050
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2023-05-01 19:20:38 -0300

    add campologin para o front

--- ingressoscometa/src/components/Login.tsx
+++ ingressoscometa/src/components/Login.tsx
@@ -1,9 +1,11 @@
 import Botao from "./Botao";
+import CampoLogin from "./CampoLogin";
 import CampoSenha from "./CampoSenha";
 
 export default function Login() {
     return(
         <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
+            <CampoLogin/>
             <CampoSenha/>
             <Botao href="/login" NomeBotao="Login"/>
         </div>
