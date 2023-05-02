2238dc16ecb49e86cc0d8a4bd938f173639d1e84
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2023-05-02 16:43:12 -0300

    Adicionando radio button de cliente adm e promote no painel de login

--- ingressoscometa/src/components/Login.tsx
+++ ingressoscometa/src/components/Login.tsx
@@ -1,12 +1,14 @@
 import Botao from "./Botao";
 import CampoCpf from "./CampoCpf";
 import CampoSenha from "./CampoSenha";
+import RadioButton from "./RadioButton";
 
 export default function Login() {
     return(
         <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
             <CampoCpf/>
             <CampoSenha/>
+            <RadioButton/>
             <Botao href="/login" NomeBotao="Login"/>
         </div>
     )
