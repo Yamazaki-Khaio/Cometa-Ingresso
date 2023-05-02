bb78bc05b32774b882926aa398f4f2d24b01c382
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2023-05-02 18:25:23 -0300

    add placeholder em password

--- ingressoscometa/src/components/CampoSenha.tsx
+++ ingressoscometa/src/components/CampoSenha.tsx
@@ -2,7 +2,7 @@ export default function CampoSenha(props: any){
     return(
         <div className="flex flex-col gap-4">
             <label htmlFor="senha">Senha</label>
-            <input type="password" name="senha" id="senha" className="border w-64 border-gray-400 rounded-md p-2 mb-8"/>
+            <input type="password" name="senha" placeholder="Insira Sua senha"  id="senha" className="border w-64 border-gray-400 rounded-md p-2 mb-8"/>
         </div>
     )
 }
