bb1484b1fea20a2e5ce1eead1fd67b64a1ce9084
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2023-05-02 18:23:13 -0300

    edititando campo email

--- ingressoscometa/src/components/CampoEmail.tsx
+++ ingressoscometa/src/components/CampoEmail.tsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
+
 export default function CampoEmail(props: any){
-    return(
-        <div className="flex flex-col gap-4">
-            <label htmlFor="senha">Email</label>
-            <input type="txt" name="email" id="email" className="border w-64 border-gray-400 rounded-md p-2 mb-8"/>
-        </div>
-    )
+  const [email, setEmail] = useState('');
+
+  function handleEmailChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setEmail(event.target.value);
+  }
+
+  return(
+    <div className="flex flex-col gap-4">
+      <label htmlFor="email">Email</label>
+      <input 
+        type="email" 
+        name="email" 
+        id="email" 
+        placeholder="Insira seu email" 
+        required 
+        className="border w-64 border-gray-400 rounded-md p-2 mb-8"
+        value={email}
+        onChange={handleEmailChange}
+      />
+    </div>
+  )
 }
