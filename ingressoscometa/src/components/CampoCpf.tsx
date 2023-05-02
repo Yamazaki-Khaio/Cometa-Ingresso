8ed9eb4eb95b898890677ee92e7e7ce5bf2d6273
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2023-05-02 15:58:14 -0300

    Validando campo cpf no login

--- ingressoscometa/src/components/CampoCpf.tsx
+++ ingressoscometa/src/components/CampoCpf.tsx
@@ -1,8 +1,46 @@
+import Botao from "./Botao";
+
+
+function isValidCPF(cpf: string) {
+    if (typeof cpf !== "string") return false
+    cpf = cpf.replace(/[\s.-]*/igm, '')
+    if (
+        !cpf ||
+        cpf.length != 11 ||
+        cpf == "00000000000" ||
+        cpf == "11111111111" ||
+        cpf == "22222222222" ||
+        cpf == "33333333333" ||
+        cpf == "44444444444" ||
+        cpf == "55555555555" ||
+        cpf == "66666666666" ||
+        cpf == "77777777777" ||
+        cpf == "88888888888" ||
+        cpf == "99999999999" 
+    ) {
+        return false
+    }
+    var soma = 0
+    var resto
+    for (var i = 1; i <= 9; i++) 
+        soma = soma + parseInt(cpf.substring(i-1, i)) * (11 - i)
+    resto = (soma * 10) % 11
+    if ((resto == 10) || (resto == 11))  resto = 0
+    if (resto != parseInt(cpf.substring(9, 10)) ) return false
+    soma = 0
+    for (var i = 1; i <= 10; i++) 
+        soma = soma + parseInt(cpf.substring(i-1, i)) * (12 - i)
+    resto = (soma * 10) % 11
+    if ((resto == 10) || (resto == 11))  resto = 0
+    if (resto != parseInt(cpf.substring(10, 11) ) ) return false
+    return true
+}
+
 export default function CampoCpf(props: any){
     return(
         <div className="flex flex-col gap-4">
             <label htmlFor="login">CPF</label>
-            <input type="text" maxLength={11} name="Cpf" id="cpf" placeholder="Insira apenas números" required pattern="\d*" className="border w-64 border-gray-400 rounded-md p-2 mb-8"/>
+            <input type="text" maxLength={14} name="Cpf" id="cpf" placeholder="Insira apenas números" required className="border w-64 border-gray-400 rounded-md p-2 mb-8"/>
         </div>
     )
 }
