a8e670d03156e15154e9b15d63ea20a87b18cb3d
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2023-05-04 17:05:00 -0300

    Opção de mostrar e esconder senha

--- ingressoscometa/src/components/CampoSenha.tsx
+++ ingressoscometa/src/components/CampoSenha.tsx
@@ -1,20 +1,39 @@
 import { useState } from "react";
+
 export default function CampoSenha(props: any){
     const [senha, setSenha] = useState('');
+    const [mostrarSenha, setMostrarSenha] = useState(false);
       
     function handleSenhaChange(event: React.ChangeEvent<HTMLInputElement>) {
         setSenha(event.target.value);
-        }
+    }
+
+    function handleMostrarSenhaChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setMostrarSenha(event.target.checked);
+    }
+
     return(
         <div className="flex flex-col gap-4">
             <label htmlFor="senha">Senha</label>
-            <input type="password" 
-                name="senha" 
-                placeholder="Insira Sua senha"  
-                id="senha" 
-                className="border w-64 border-gray-400 rounded-md p-2 mb-8"
-                onChange={handleSenhaChange}
+            <div className="relative">
+                <input 
+                    type={mostrarSenha ? "text" : "password"} // alterna entre exibir a senha como texto e como um valor oculto
+                    name="senha" 
+                    placeholder="Insira Sua senha"  
+                    id="senha" 
+                    className="border w-64 border-gray-400 rounded-md p-2 mb-2"
+                    onChange={handleSenhaChange}
                 />
+                <label htmlFor="mostrarSenha" className="flex items-center">
+                    <input 
+                        type="checkbox" 
+                        id="mostrarSenha" 
+                        className="mr-2"
+                        onChange={handleMostrarSenhaChange}
+                    />
+                    <span>Mostrar senha</span>
+                </label>
+            </div>
         </div>
     )
 }
