1c6baff5fa7b87f87fa5ce216e13032e0c62c4af
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2023-05-04 14:13:08 -0300

    Revert "Arredondando botão"
    This reverts commit 0fe7ea27888f3529da60178d012e087ac52d80e5.

--- ingressoscometa/src/components/botao.tsx
+++ ingressoscometa/src/components/botao.tsx
@@ -4,22 +4,16 @@ export default function Botao(props: {href: string, NomeBotao: string }){
     return(
         <div>
             <Link href={props.href}>
-                <button className={`
+                    <button className={`
                     w-64 h-12
-                    bg-teal-900 
-                    text-white 
-                    text-24 
-                    rounded-full
-                `} role="button">
-                    {props.NomeBotao}
-                </button>
+                     bg-teal-900 
+                     text-white 
+                     text-24 
+                     rounded-xl`}  role="button">
+                        {props.NomeBotao}
+                    </button>
             </Link>
         </div>
+        
     )
 }
-
-
-
-
-
-
