2a3df3c05570b94502ec7ff2e98ad975be73cbb1
Alisson Bomfim <alissonbomfimsilva@outlook.com>
2023-05-04 19:44:59 -0300

    modificando componente sessaão

--- ingressoscometa/src/components/sessao.tsx
+++ ingressoscometa/src/components/sessao.tsx
@@ -1,18 +1,27 @@
 import { signOut,useSession } from "next-auth/react";
+import Router, { useRouter } from "next/router";
+
 
 export default function Sessao(){
     const { data: session } = useSession()
-    return(
-        <div>
-            <button  className={`
-                    w-64 h-12
-                     bg-teal-900 
-                     text-white 
-                     text-24 
-                    rounded-xl`}  role="button" 
-                    onClick={() => {signOut({ callbackUrl: 'http://localhost:3000/login' })}} >Sair</button>
-                    <h1>olá {session?.user?.name}, Bem vindo</h1>
+    const route = useRouter()
+    if(session){
+        return(
+            <div>
+                <button  className={`
+                        w-64 h-12
+                        bg-teal-900 
+                        text-white 
+                        text-24 
+                        rounded-xl`}  role="button" 
+                        onClick={() => {signOut({ callbackUrl: 'http://localhost:3000/login' })}} >Sair</button>
+                        <h1>olá {session?.user?.name}, Bem vindo</h1>
 
-        </div>
-    )
+            </div>
+        )
+    }
+   if (process.browser){
+        route.replace('/login')
+    }
+    
 }
