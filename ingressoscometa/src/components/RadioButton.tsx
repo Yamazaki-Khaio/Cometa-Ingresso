1043f867d76215e5a55ab3a29aa693cc81647852
lipealves13 <lipealves22@gmail.com>
2023-05-04 17:52:05 -0300

    margem do RadioButton

--- ingressoscometa/src/components/RadioButton.tsx
+++ ingressoscometa/src/components/RadioButton.tsx
@@ -1,6 +1,6 @@
 export default function RadioButton(props: any){
     return(
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 mt-2 mb-4">
             <div className="flex gap-2">
                 <div className="flex items-center gap-2">
                     <input type="radio" name="tipoUsuario" id="cliente" value="cliente"/>
