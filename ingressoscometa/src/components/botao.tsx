7da086858ba996bb38a00faafd58b938c1d3e4db
Alana Sampaio <alana.sampaio2000@gmail.com>
2023-05-04 22:21:35 -0300

    ocultando botão login da tela de login, permanecendo somente o cadastro

--- ingressoscometa/src/components/botao.tsx
+++ ingressoscometa/src/components/botao.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link"
 
-export default function Botao(props: {href: string, NomeBotao: string }){
+export default function Botao(props: {href: string, NomeBotao: string, oculto?: boolean}){
+
+    if (props.oculto) {
+        return null;
+    }
+
     return(
         <div>
             <Link href={props.href}>
-                    <button className={`
-                    w-48 h-12  // diminuindo o comprimento do botão
+                <button className={`
+                     w-48 h-12  // diminuindo o comprimento do botão
                      bg-teal-900 
                      text-white 
                      text-24 
@@ -13,12 +18,11 @@ export default function Botao(props: {href: string, NomeBotao: string }){
                      transition-all duration-300  // adicionando um efeito de transição
                      hover:bg-teal-800  // adicionando uma cor de fundo quando o botão é hoverado
                      active:bg-teal-700  // adicionando uma cor de fundo quando o botão é clicado
-                     `}
-                     role="button">
-                        {props.NomeBotao}
-                    </button>
+                `}
+                role="button">
+                    {props.NomeBotao}
+                </button>
             </Link>
         </div>
-        
     )
 }
--- ingressoscometa/src/components/layout.tsx
+++ ingressoscometa/src/components/layout.tsx
@@ -3,12 +3,10 @@ import Rodope from "./Rodope"
 
 export default function Layout(props: any){
     return(
-        
         <div className="flex flex-col justify-between h-screen">
-            <Cabecalho/> 
+            <Cabecalho isBotaoOcultoLogin={props.exibirBotao === false} isBotaoOcultoCadastro={props.exibirBotao2 === false}/> 
             {props.pagina}
             <Rodope/>
         </div>
     )
-
 }
