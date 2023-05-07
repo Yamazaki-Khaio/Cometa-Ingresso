f4769f91ad16b5ff7b6e147e9939c22fd42540dd
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2023-05-07 19:45:48 -0300

    Adicionando click ao logo para redirencionar para tela inicial

--- ingressoscometa/src/components/Cabecalho.tsx
+++ ingressoscometa/src/components/Cabecalho.tsx
@@ -5,7 +5,9 @@ export default function Cabecalho(props: { isBotaoOcultoLogin?: boolean,  isBota
     return(
         <div className="flex items-center justify-between p-8 w-screen h-32 bg-teal-300">
             <div className="flex items-center gap-4">
-                <img src="/cometa2.png" alt="Logo" width="200" height="200"/>
+                <a href="/">
+                    <img src="/cometa2.png" alt="Logo" width="200" height="200"/>
+                </a>
                 <h2>Compre seus ingressos aqui!</h2>  
             </div>
             <div className="flex gap-4">
