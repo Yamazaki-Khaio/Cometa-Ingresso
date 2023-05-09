5308e2c01ad01e51348d29eff871337828a89081
OinotnaEpilef <felipetompsomf18@gmail.com>
2023-05-09 15:26:14 -0300

    refactor rodape

--- ingressoscometa/src/components/Rodape.tsx
+++ ingressoscometa/src/components/Rodape.tsx
@@ -1,4 +1,4 @@
-export default function Rodope(){
+export default function Rodape(){
     return(
         <div>
             <div className="flex items-center justify-between p-8 w-screen h-32 bg-teal-300 ">
