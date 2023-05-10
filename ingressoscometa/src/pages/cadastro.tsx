e6dbb11533d2a55e4f8a8266ca79be1598f9c975
Yamazaki Khaio <kaio_7fsa@hotmail.com>
2023-05-10 12:26:10 -0300

    ajeitando o nome do arquivo layout para Layout

--- ingressoscometa/src/pages/cadastro.tsx
+++ ingressoscometa/src/pages/cadastro.tsx
@@ -1,6 +1,6 @@
 
 import Cadastro from "@/components/Cadastro";
-import Layout from "@/components/layout";
+import Layout from "@/components/Layout";
 
 export default function cadastro(){
     return(
