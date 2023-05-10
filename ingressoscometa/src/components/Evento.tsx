22210486b6912a7c854b824697ffed16918f0d90
lipealves13 <lipealves22@gmail.com>
2023-05-10 15:41:11 -0300

    componente de evento da home

--- ingressoscometa/src/components/Evento.tsx
+++ ingressoscometa/src/components/Evento.tsx
@@ -1,11 +1,16 @@
 export default function Evento(props:any) {
     return (
-        <div className="bg-red-700">
-            <img src={props.Image}/>
-            <p>{props.Nome}</p>
-            <p>Data: {props.Data}</p>
-            <p>A partir das: {props.Hora}</p>
-            <p>{props.Local}</p>
+        <div className="flex flex-col bg-zinc-300 h-64 w-80 border border-zinc-400 rounded-2xl">
+
+            <div className="relative mx-auto rounded-2xl overflow-hidden">
+                <img src={props.Image}/>
+            </div>
+            <div className="p-4">
+                <p className="font-sans text-md">{props.Nome}</p>
+                <p className="font-sans text-sm">Data: {props.Data}</p>
+                <p className="font-sans text-sm">A partir das: {props.Hora}</p>
+                <p className="font-sans text-sm">{props.Local}</p>
+            </div>
 
         </div>
     )}
--- ingressoscometa/src/components/GrindEvento.tsx
+++ ingressoscometa/src/components/GrindEvento.tsx
@@ -2,12 +2,14 @@ import Evento from "./Evento"
 
 export default function GridEvento(props:any) {
     return (
-        <div className="flex flex-wrap">
-            <Evento Nome={'Justin Bieber Tour'} Local={'Casa de Khaio'} Data={'2023-05-27'} Image={'cometa2.png'} />
-
-
-            
-
-            <Evento/>
+        <div className="flex flex-wrap gap-4 justify-center items-center p-12">
+            <Evento Nome={'Justin Bieber Tour'} Local={'Casa de Khaio'} Data={'2023-05-27'} Image={'Evento-Corporativo.jpg'} />
+            <Evento Nome={'Justin Bieber Tour'} Local={'Casa de Khaio'} Data={'2023-05-27'} Image={'Evento-Corporativo.jpg'} />
+            <Evento Nome={'Justin Bieber Tour'} Local={'Casa de Khaio'} Data={'2023-05-27'} Image={'Evento-Corporativo.jpg'} />
+            <Evento Nome={'Justin Bieber Tour'} Local={'Casa de Khaio'} Data={'2023-05-27'} Image={'Evento-Corporativo.jpg'} />
+            <Evento Nome={'Justin Bieber Tour'} Local={'Casa de Khaio'} Data={'2023-05-27'} Image={'Evento-Corporativo.jpg'} />
+            <Evento Nome={'Justin Bieber Tour'} Local={'Casa de Khaio'} Data={'2023-05-27'} Image={'Evento-Corporativo.jpg'} />
+            <Evento Nome={'Justin Bieber Tour'} Local={'Casa de Khaio'} Data={'2023-05-27'} Image={'Evento-Corporativo.jpg'} />
+            <Evento Nome={'Justin Bieber Tour'} Local={'Casa de Khaio'} Data={'2023-05-27'} Image={'Evento-Corporativo.jpg'} />
         </div>
     )}
