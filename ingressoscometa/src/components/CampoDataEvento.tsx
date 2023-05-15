0f183a670cb4b56195b82d67a7b510fd34ff2de5
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2023-05-15 15:39:24 -0300

    Adicionando datepicker

--- ingressoscometa/src/components/CampoDataEvento.tsx
+++ ingressoscometa/src/components/CampoDataEvento.tsx
@@ -1,25 +1,25 @@
 import { useState } from "react";
 
-export default function CampoDataEvento(props: any){
+export default function CampoDataEvento(props: any) {
   const [data, setData] = useState('');
 
   function handleDataChange(event: React.ChangeEvent<HTMLInputElement>) {
     setData(event.target.value);
   }
 
-  return(
+  return (
     <div className="flex flex-col gap-4">
       <label htmlFor="data">Data</label>
-      <input 
-        type="data" 
-        name="data" 
-        id="data" 
-        placeholder="  /  /  " 
-        required 
+      <input
+        type="date"
+        name="data"
+        id="data"
+        placeholder="  /  /  "
+        required
         className="border w-64 border-gray-400 rounded-md p-2 mb-8"
         value={data}
         onChange={handleDataChange}
       />
     </div>
-  )
+  );
 }
