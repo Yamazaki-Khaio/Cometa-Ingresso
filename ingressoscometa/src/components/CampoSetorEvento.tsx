147c43990aef9b6040edfc8784089b36caa42df2
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2023-05-15 15:46:59 -0300

    Adicionando choicebox setor

--- ingressoscometa/src/components/CampoSetorEvento.tsx
+++ ingressoscometa/src/components/CampoSetorEvento.tsx
@@ -1,25 +1,28 @@
 import { useState } from "react";
 
-export default function CampoSetorEvento(props: any){
+export default function CampoSetorEvento(props: any) {
   const [setor, setSetor] = useState('');
 
-  function handleSetorChange(event: React.ChangeEvent<HTMLInputElement>) {
+  function handleSetorChange(event: React.ChangeEvent<HTMLSelectElement>) {
     setSetor(event.target.value);
   }
 
-  return(
+  return (
     <div className="flex flex-col gap-4">
       <label htmlFor="setor">Setor</label>
-      <input 
-        type="setor" 
-        name="setor" 
-        id="setor" 
-        placeholder="Insira o setor do evento" 
-        required 
+      <select
+        name="setor"
+        id="setor"
+        required
         className="border w-64 border-gray-400 rounded-md p-2 mb-8"
         value={setor}
         onChange={handleSetorChange}
-      />
+      >
+        <option value="">Selecione o setor</option>
+        <option value="VIP">VIP</option>
+        <option value="Backstage">Backstage</option>
+        <option value="Camarote">Camarote</option>
+      </select>
     </div>
-  )
+  );
 }
