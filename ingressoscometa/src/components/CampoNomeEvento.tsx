79094177bee49dc0a901abd766ea5f104491a653
Pedro Santos <psgbi123@gmail.com>
2023-05-16 16:18:27 -0300

    Modificando o componente CampoNomeEvento

--- ingressoscometa/src/components/CampoNomeEvento.tsx
+++ ingressoscometa/src/components/CampoNomeEvento.tsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
 
 export default function CampoNomeEvento(props: any){
+  const [nome, setNome] = useState(""); //Armazena o valor do nome do evento
+  function handleNomeChange(event: React.ChangeEvent<HTMLInputElement>){//Modifica o valor do nome do
+    setNome(event.target.value);
+  }
   return(
     <div className="flex flex-col gap-4">
       <label htmlFor="nome">Nome</label>
       <input 
         id = "nome"
-        type='text'
+        type="text"
         placeholder="Insira o nome do evento" 
         className="border w-64 border-gray-400 rounded-md p-2 mb-8"
+        value={nome}
+        onChange={handleNomeChange}
       />
     </div>
   )
