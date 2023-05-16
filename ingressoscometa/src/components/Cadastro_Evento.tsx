e44d40878e73381b1692706192eab8521e8d959a
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2023-05-16 14:09:41 -0300

    Arrumando alguns componentes de Criar evento

--- ingressoscometa/src/components/Cadastro_Evento.tsx
+++ ingressoscometa/src/components/Cadastro_Evento.tsx
@@ -11,51 +11,93 @@ import Evento from "./Evento";
 import { Input } from "postcss";
 
 export default function CadastroEvento() {
-    const [formData, setformData] = useState({
-        nome:'',
-        desc:'',
-        local:'',
-        data:'',
-        horario:'',
-        perfil:'',
-        setor:''
-    }
-    );
+  const [formData, setFormData] = useState({
+    nome: "",
+    desc: "",
+    local: "",
+    data: "",
+    horario: "",
+    perfil: "",
+    setor: "",
+  });
 
-    const handleformEdit = (event, name) =>{
-        setformData({
-            ...formData, 
-            [name]: event.target.value
-        })
-    }
+  const handleFormEdit = (event, name) => {
+    setFormData({
+      ...formData,
+      [name]: event.target.value,
+    });
+  };
 
-    const handle = async (e) => {
-        try{
-            e.preventDefault()
-            const res = await fetch('/api/evento', {
-                method: 'POST',
-                body: JSON.stringify(formData)
-            })
-            const json = await res.json()
-            console.log(res.status)
-            console.log(json)
-            console.log(formData)
-        } catch(err){
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const res = await fetch("/api/evento", {
+        method: "POST",
+        body: JSON.stringify(formData),
+      });
+      const json = await res.json();
+      console.log(res.status);
+      console.log(json);
+      console.log(formData);
+    } catch (err) {}
+  };
 
-    }
-}
-return(
-        <div className="flex flex-col justify-center items-center p-12 bg-gray-100">
-            <form onSubmit={handle}>
-            <CampoNomeEvento  required value = {formData.nome} onChange={(e) => {handleformEdit(e, 'nome')}}/>
-            <CampoDescricaoEvento required value = {formData.desc} onChange={(e) => {handleformEdit(e, 'desc')}}/>
-            <CampoLocal required value = {formData.local} onChange={(e) => {handleformEdit(e, 'local')}}/>
-            <CampoDataEvento required value = {formData.data} onChange={(e) => {handleformEdit(e, 'data')}}/>
-            <CampoHorarioEvento required value = {formData.horario} onChange={(e) => {handleformEdit(e, 'horario')}}/>
-            <CampoPerfilEvento required value = {formData.perfil} onChange={(e) => {handleformEdit(e, 'perfil')}}/>
-            <CampoSetorEvento required value = {formData.setor} onChange={(e) => {handleformEdit(e, 'setor')}}/>
-            <BotaoSubmitEvento/>
-            </form>
+  return (
+    <div className="flex flex-col justify-center items-center p-12 bg-gray-100">
+      <form onSubmit={handleSubmit}>
+        <CampoNomeEvento
+          required
+          value={formData.nome}
+          onChange={(e) => {
+            handleFormEdit(e, "nome");
+          }}
+        />
+        <CampoDescricaoEvento
+          required
+          value={formData.desc}
+          onChange={(e) => {
+            handleFormEdit(e, "desc");
+          }}
+        />
+        <CampoLocal
+          required
+          value={formData.local}
+          onChange={(e) => {
+            handleFormEdit(e, "local");
+          }}
+        />
+        <CampoPerfilEvento
+          required
+          value={formData.perfil}
+          onChange={(e) => {
+            handleFormEdit(e, "perfil");
+          }}
+        />
+        <CampoSetorEvento
+          required
+          value={formData.setor}
+          onChange={(e) => {
+            handleFormEdit(e, "setor");
+          }}
+        />
+        <div className="flex flex-row gap-4">
+          <CampoDataEvento
+            required
+            value={formData.data}
+            onChange={(e) => {
+              handleFormEdit(e, "data");
+            }}
+          />
+          <CampoHorarioEvento
+            required
+            value={formData.horario}
+            onChange={(e) => {
+              handleFormEdit(e, "horario");
+            }}
+          />
         </div>
-    )
+        <BotaoSubmitEvento />
+      </form>
+    </div>
+  );
 }
