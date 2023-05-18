48ba63e216062edd573eb5ce17629909bdc2a9ef
Alisson Bomfim <alissonbomfimsilva@outlook.com>
2023-05-18 13:12:56 -0300

    implementando rota para telefone

---
+++ ingressoscometa/src/pages/api/telefone.tsx
@@ -0,0 +1,67 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import {connection} from './db';
+
+export default function handler(req: NextApiRequest,res: NextApiResponse){
+    //caso metodo get
+    if(req.method==='GET'){
+        //caso não haver paramentros
+        if(!req.query){
+            const sql = 'SELECT * FROM telefone'
+            try{
+                connection.query(sql, (error, results, fields) => {
+                    res.status(200).send(results)
+                    return
+                })
+            }catch(error){
+                console.error('Erro ao buscar telefones: ', error);
+                res.status(500).send('Erro ao buscar telefones.');
+            }
+          
+        }else{
+            //se não
+            //busca por telefone
+            if(req.query['telefone']){
+                const sql = 'SELECT * FROM telefone WHERE telefone=?'
+                try{
+                    connection.query(sql,[req.query['telefone']], (error, results, fields) => {
+                        res.status(200).send(results)
+                        return;
+                    })
+                }catch(error){
+                    console.error('Erro ao buscar telefones: ', error);
+                    res.status(500).send('Erro ao buscar telefones.');
+                }
+
+            }
+
+            // busca por id de telefone
+            if(req.query['id']){
+                try{
+                    const sql = 'SELECT * FROM telefone WHERE id=?'
+                    connection.query(sql,[req.query['id']], (error, results, fields) => {
+                        res.status(200).send(results)
+                        return;
+                    })
+                }catch(error){
+                    console.error('Erro ao buscar telefones: ', error);
+                    res.status(500).send('Erro ao buscar telefones.');
+                }
+            }
+            //busca por id de celular 
+            if(req.query['id_usuario']){
+                try{
+                    const sql = 'SELECT * FROM telefone WHERE id_usuario=?'
+                    connection.query(sql,[req.query['id_usuario']], (error, results, fields) => {
+                        res.status(200).send(results)
+                        return;
+                    })
+                }catch(error){
+                    console.error('Erro ao buscar telefones: ', error);
+                    res.status(500).send('Erro ao buscar telefones.');
+                }
+            }
+        }
+
+    }
+    
+}
