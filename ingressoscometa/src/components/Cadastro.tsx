d24f6301b39a6aaf608e49e35e2cd5b0db7a99df
Pedro Cruz <pedrohenvcruz@gmail.com>
2023-05-19 20:39:35 -0300

    rota de email

--- ingressoscometa/src/components/Cadastro.tsx
+++ ingressoscometa/src/components/Cadastro.tsx
@@ -118,7 +118,7 @@ export default function Cadastro() {
           name="numeroCasa"
         />
         <CampoDataDeNascimento
-          value={formData.numeroCasa}
+          value={formData.data_nascimento}
           onChange={handleInputChange}
           name="data_nascimento"
         />
