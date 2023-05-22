0e5c0a60695924bebb3646f027d65421480d03cf
Pedro Cruz <pedrohenvcruz@gmail.com>
2023-05-22 18:01:04 -0300

    Finalizar compra

---
+++ ingressoscometa/src/components/Tela Finalizar Compra/FinalizarCompra.tsx
@@ -0,0 +1,13 @@
+export default function FinalizarCompra() {
+    return (
+        <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
+            <form>
+                
+            </form>
+
+
+
+
+        </div>
+    )
+}
