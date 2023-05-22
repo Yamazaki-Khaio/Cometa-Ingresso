a6bd7bf6303ff2b2fb07fb4163f1f58ef8836d77
lipealves13 <lipealves22@gmail.com>
2023-05-22 13:32:39 -0300

    arrumando rotas

--- ingressoscometa/src/pages/api/endereco.tsx
+++ ingressoscometa/src/pages/api/endereco.tsx
@@ -2,75 +2,75 @@ import express from 'express';
 import { connection } from './db';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const router = express.Router();
+const router = express.Router();
 
-    router.use('/', async (req, res) => {
-        if (req.method === 'POST') {
-            // Cadastrar endereço
-            const { cep, rua, numero, complemento, UsuarioID } = req.headers;
-            const sql =
-                'INSERT INTO endereco (cep, rua, numero, complemento, UsuarioID) VALUES (?, ?, ?, ?, ?)';
-            const params = [cep, rua, numero, complemento, UsuarioID];
-            connection.query(sql, params, (error, results, fields) => {
-                if (error) {
-                    console.error('Erro ao cadastrar endereço:', error);
-                    res.status(500).send('Erro ao cadastrar endereço.');
-                    return;
-                }
-                res.json(results);
-            });
-        } else if (req.method === 'GET') {
-            if (req.query && req.query.id) {
-                // Obter endereço por ID
-                const sql = 'SELECT * FROM endereco WHERE id_endereco=?';
-                connection.query(sql, [req.query.id], (error, results, fields) => {
-                    if (error) {
-                        console.error('Erro ao buscar endereço:', error);
-                        res.status(500).send('Erro ao buscar endereço.');
-                        return;
-                    }
-                    res.json(results);
-                });
-            } else {
-                // Obter todos os endereços
-                const sql = 'SELECT * FROM endereco';
-                connection.query(sql, (error, results, fields) => {
-                    if (error) {
-                        console.error('Erro ao buscar endereços:', error);
-                        res.status(500).send('Erro ao buscar endereços.');
-                        return;
-                    }
-                    res.json(results);
-                });
+router.use('/', async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'POST') {
+        // Cadastrar endereço
+        const { cep, rua, numero, complemento, UsuarioID } = req.headers;
+        const sql =
+            'INSERT INTO endereco (cep, rua, numero, complemento, UsuarioID) VALUES (?, ?, ?, ?, ?)';
+        const params = [cep, rua, numero, complemento, UsuarioID];
+        connection.query(sql, params, (error, results, fields) => {
+            if (error) {
+                console.error('Erro ao cadastrar endereço:', error);
+                res.status(500).send('Erro ao cadastrar endereço.');
+                return;
             }
-        } else if (req.method === 'PUT') {
-            // Atualizar endereço
-            const { cep, rua, numero, complemento, UsuarioID } = req.headers;
-            const sql =
-                'UPDATE endereco SET cep=?, rua=?, numero=?, complemento=?, UsuarioID=? WHERE id_endereco=?';
-            const params = [cep, rua, numero, complemento, UsuarioID, req.query.id];
-            connection.query(sql, params, (error, results, fields) => {
-                if (error) {
-                    console.error('Erro ao atualizar endereço:', error);
-                    res.status(500).send('Erro ao atualizar endereço.');
-                    return;
-                }
-                res.json(results);
-            });
-        } else if (req.method === 'DELETE') {
-            // Deletar endereço
-            const sql = 'DELETE FROM endereco WHERE id_endereco=?';
+            res.json(results);
+        });
+    } else if (req.method === 'GET') {
+        if (req.query && req.query.id) {
+            // Obter endereço por ID
+            const sql = 'SELECT * FROM endereco WHERE id_endereco=?';
             connection.query(sql, [req.query.id], (error, results, fields) => {
                 if (error) {
-                    console.error('Erro ao deletar endereço:', error);
-                    res.status(500).send('Erro ao deletar endereço.');
+                    console.error('Erro ao buscar endereço:', error);
+                    res.status(500).send('Erro ao buscar endereço.');
                     return;
                 }
                 res.json(results);
             });
         } else {
-            res.status(404).send('Rota não encontrada.');
+            // Obter todos os endereços
+            const sql = 'SELECT * FROM endereco';
+            connection.query(sql, (error, results, fields) => {
+                if (error) {
+                    console.error('Erro ao buscar endereços:', error);
+                    res.status(500).send('Erro ao buscar endereços.');
+                    return;
+                }
+                res.json(results);
+            });
         }
-    });
-}
+    } else if (req.method === 'PUT') {
+        // Atualizar endereço
+        const { cep, rua, numero, complemento, UsuarioID } = req.headers;
+        const sql =
+            'UPDATE endereco SET cep=?, rua=?, numero=?, complemento=?, UsuarioID=? WHERE id_endereco=?';
+        const params = [cep, rua, numero, complemento, UsuarioID, req.query.id];
+        connection.query(sql, params, (error, results, fields) => {
+            if (error) {
+                console.error('Erro ao atualizar endereço:', error);
+                res.status(500).send('Erro ao atualizar endereço.');
+                return;
+            }
+            res.json(results);
+        });
+    } else if (req.method === 'DELETE') {
+        // Deletar endereço
+        const sql = 'DELETE FROM endereco WHERE id_endereco=?';
+        connection.query(sql, [req.query.id], (error, results, fields) => {
+            if (error) {
+                console.error('Erro ao deletar endereço:', error);
+                res.status(500).send('Erro ao deletar endereço.');
+                return;
+            }
+            res.json(results);
+        });
+    } else {
+        res.status(404).send('Rota não encontrada.');
+    }
+});
+
+export default router;
--- ingressoscometa/src/pages/api/usuario.tsx
+++ ingressoscometa/src/pages/api/usuario.tsx
@@ -1,16 +1,17 @@
 import express from 'express';
-import {connection} from './db';
-import Evento from './classes/Usuario';
+import { connection } from './db';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { randomInt } from 'crypto';
 
-export default async function handler(req: NextApiRequest,res: NextApiResponse){
-    const router = express.Router();
-    var x = randomInt(1000)
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const router = express.Router();
+  var x = randomInt(1000);
+
   router.use('/', async (req, res) => {
     if (req.method === 'POST') {
-      // Criar usuario
-      const sql = "INSERT into usuario (id, cpf, nome, senha, data_nascimento, tipo_user) VALUES (?, ?, ?, ?, ?, ?)";
+      // Criar usuário
+      const sql =
+        'INSERT INTO usuario (id, cpf, nome, senha, data_nascimento, tipo_user) VALUES (?, ?, ?, ?, ?, ?)';
       const params = [
         x,
         req.body.cpf,
@@ -19,89 +20,82 @@ export default async function handler(req: NextApiRequest,res: NextApiResponse){
         req.body.data_nascimento,
         '1',
       ];
-            connection.query(sql, params, (error, results, fields) => {
-                if (error) {
-                console.error('Erro ao inserir novo usuario', error);
-                res.status(500).send('Erro ao inserir novo usuario.');
-                return;
-                }
-                res.json(results);
-        });
-        } else if (req.method === 'GET') {
-          if(!req.query){
-            const sql = 'SELECT * FROM usuario';
-            connection.query(sql, (error, results, fields) => {
-              if (error) {
-                console.error('Erro ao buscar usuario: ', error);
-                res.status(500).send('Erro ao buscar usuario.');
-                return;
-              }
-              res.json(results);
-            });
-          }
-
-          if(req.query['cpf']){
-            const sql = 'SELECT * FROM usuario WHERE cpf=?';
-            connection.query(sql,[req.query['cpf']],(error, results, fields) => {
-              if (error) {
-                console.error('Erro ao buscar usuario: ', error);
-                res.status(500).send('Erro ao buscar usuario.');
-                return;
-              }
-              res.json(results);
-            });
-          }
-
-          if(req.query['id']){
-            const sql = 'SELECT * FROM usuario WHERE id=?';
-            connection.query(sql,[req.query['id']],(error, results, fields) => {
-              if (error) {
-                console.error('Erro ao buscar usuario: ', error);
-                res.status(500).send('Erro ao buscar usuario.');
-                return;
-              }
-              res.json(results);
-            });
-          }
-
-          const sql = 'SELECT * FROM usuario';
-          connection.query(sql, (error, results, fields) => {
-          if (error) {
-            console.error('Erro ao buscar usuario: ', error);
-            res.status(500).send('Erro ao buscar usuario.');
-                return;
-            }
-            res.json(results);
-            });
-          
-        } else if (req.method === 'DELETE') {
-          // Remover usuário
-          const sql = 'DELETE FROM usuario WHERE id=?';
-          connection.query(sql, [req.body.idUser], (error, results, fields) => {
-            if (error) {
-              console.error('Erro ao remover usuario: ', error);
-              res.status(500).send('Erro ao remover usuario.');
-              return;
-            }
-            res.json(results);
-          });
-        } else if (req.method === 'PUT') {
-          // Atualizar usuario
-          const sql = 'UPDATE evento SET ? WHERE id=?';
-          connection.query(
-            sql,
-            [req.body, req.query.id],
-            (error, results, fields) => {
-              if (error) {
-                console.error('Erro ao atualizar usuario: ', error);
-                res.status(500).send('Erro ao atualizar usuario.');
-                return;
-              }
-              res.json(results);
-            }
-          );
-        } else {
-          res.status(404).send('Rota não encontrada.');
+      connection.query(sql, params, (error, results, fields) => {
+        if (error) {
+          console.error('Erro ao inserir novo usuário', error);
+          res.status(500).send('Erro ao inserir novo usuário.');
+          return;
         }
+        res.json(results);
       });
+    } else if (req.method === 'GET') {
+      if (!req.query) {
+        const sql = 'SELECT * FROM usuario';
+        connection.query(sql, (error, results, fields) => {
+          if (error) {
+            console.error('Erro ao buscar usuário: ', error);
+            res.status(500).send('Erro ao buscar usuário.');
+            return;
+          }
+          res.json(results);
+        });
+      } else if (req.query['cpf']) {
+        const sql = 'SELECT * FROM usuario WHERE cpf=?';
+        connection.query(sql, [req.query['cpf']], (error, results, fields) => {
+          if (error) {
+            console.error('Erro ao buscar usuário: ', error);
+            res.status(500).send('Erro ao buscar usuário.');
+            return;
+          }
+          res.json(results);
+        });
+      } else if (req.query['id']) {
+        const sql = 'SELECT * FROM usuario WHERE id=?';
+        connection.query(sql, [req.query['id']], (error, results, fields) => {
+          if (error) {
+            console.error('Erro ao buscar usuário: ', error);
+            res.status(500).send('Erro ao buscar usuário.');
+            return;
+          }
+          res.json(results);
+        });
+      } else {
+        const sql = 'SELECT * FROM usuario';
+        connection.query(sql, (error, results, fields) => {
+          if (error) {
+            console.error('Erro ao buscar usuário: ', error);
+            res.status(500).send('Erro ao buscar usuário.');
+            return;
+          }
+          res.json(results);
+        });
+      }
+    } else if (req.method === 'DELETE') {
+      // Remover usuário
+      const sql = 'DELETE FROM usuario WHERE id=?';
+      connection.query(sql, [req.body.idUser], (error, results, fields) => {
+        if (error) {
+          console.error('Erro ao remover usuário: ', error);
+          res.status(500).send('Erro ao remover usuário.');
+          return;
+        }
+        res.json(results);
+      });
+    } else if (req.method === 'PUT') {
+      // Atualizar usuário
+      const sql = 'UPDATE evento SET ? WHERE id=?';
+      connection.query(sql, [req.body, req.query.id], (error, results, fields) => {
+        if (error) {
+          console.error('Erro ao atualizar usuário: ', error);
+          res.status(500).send('Erro ao atualizar usuário.');
+          return;
+        }
+        res.json(results);
+      });
+    } else {
+      res.status(404).send('Rota não encontrada.');
     }
+  });
+
+  router(req, res); // Invoke the router with req and res objects
+}
