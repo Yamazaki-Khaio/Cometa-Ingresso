35ee5399ff584ccaa4e736a5c2b34af243b60ab5
Pedro Cruz <pedrohenvcruz@gmail.com>
2023-05-23 19:59:53 -0300

    finalizar compra

--- ingressoscometa/src/components/FinalizarCompra/BotaoCancelarCompra.tsx
+++ ingressoscometa/src/components/FinalizarCompra/BotaoCancelarCompra.tsx
@@ -4,10 +4,10 @@ export default function BotaoCancelarCompra(){
         <div>
             <input type="cancelar" value="            Cancelar" className={`
                     flex flex-col items-center justify-center
-                     w-25 h-12  // diminuindo o comprimento do botão
+                    w-25 h-12  // diminuindo o comprimento do botão
                      bg-gray-400 
                      text-black 
-                     text-24 
+                     text-10
                      rounded-3xl  // aumentando o valor do border-radius
                      transition-all duration-300  // adicionando um efeito de transição
                      hover:white-800  // adicionando uma cor de fundo quando o botão é hoverado
