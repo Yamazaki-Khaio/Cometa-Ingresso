691f682707e21251a95d1814fc322d13415176ae
Yamazaki Khaio <kfonseca@ecomp.uefs.br>
2023-05-24 00:40:36 -0300

    alteração n

--- ingressoscometa/src/components/DestaqueSemana.tsx
+++ ingressoscometa/src/components/DestaqueSemana.tsx
@@ -4,42 +4,41 @@ import Evento from './Evento'
 import { eventos } from './GrindEvento'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
+import { ALL } from 'dns'
 
 const settings = {
-  className: "center",
+  className: "center mx-5",
   dots: false,
   infinite: true,
-  centerPadding: "60px",
+  centerPadding: "50px",
   speed: 2000,
   slidesToShow: 3,
-  slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 3000,
   cssEase: "linear",
   adaptiveHeight: true,
   pauseOnHover: true,
-
+  initialSlide: 0,
+  sliderWidth: 100,
+  slideMargin: 100,
+  
   
 }
 
 export default function DestaqueSemana(props: any) {
   return (
-    <div className="relative flex-wrap gap-5 justify-center items-center p-4">
+    <div className="relative flex-wrap justify-center items-center p-4 gap-4">
       <Slider {...settings}>
         {eventos.map((eventoDestaque, index) => (
-            <div key={index} className="destaque-evento">
-          <Evento
-            key={index}
-            Nome={eventoDestaque.Nome}
-            Data={eventoDestaque.Data}
-            Hora={eventoDestaque.Hora}
-            Local={eventoDestaque.Local}
-            Image={eventoDestaque.Image}
-            className="w-full h-full"
-          />
+          <div key={index} className="destaque-evento">
+            <img
+              src={eventoDestaque.Image}
+              alt={eventoDestaque.Nome}
+              className="w-[500px] h-[300px] object-cover rounded-xl border-separate border-4 border-white"
+            />
           </div>
         ))}
       </Slider>
     </div>
-  )
+  );
 }
