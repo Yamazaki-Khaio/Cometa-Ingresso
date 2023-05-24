4071df9b68c0ed7f3fea17b9b0ca5d5117c81058
Pedro Cruz <pedrohenvcruz@gmail.com>
2023-05-24 16:44:26 -0300

    fin compra fix

--- ingressoscometa/src/components/FinalizarCompra/FinalizarCompra.tsx
+++ ingressoscometa/src/components/FinalizarCompra/FinalizarCompra.tsx
@@ -3,18 +3,18 @@ import BotaoCancelarCompra from "./BotaoCancelarCompra"
 
 export default function FinalizarCompra() {
     return (
-        <div className="flex h-screen justify-center items-center flex-col">
+        <div class="flex h-screen justify-center items-center flex-col">
     <div
-        className=" w-1/4 h-80 bg-[url('http://localhost:3000/')] bg-gray-100 bg-center">
-        <div className="w-full h-full flex items-center justify-center bg-white backdrop-brightness-50">
-            <div classNames="absolute bottom-0 left-30 ...">
+        class=" w-1/4 h-80 bg-[url('http://localhost:3000/')] bg-gray-100 bg-center">
+        <div class="w-full h-full flex items-center justify-center bg-white backdrop-brightness-50">
+            <div class="absolute bottom-0 right-0 ...">
             <BotaoConfirmarCompra/>
             </div>
-            <div classNamess="absolute bottom-0 right-0 ...">
+            <div class="absolute bottom-0 left-0 ...">
             <BotaoCancelarCompra/>
             </div>
                 
-                <div classNamess="absolute left-30 top-10 text-xl ...">Deseja confirmar a compra?</div>
+                <div class="absolute left-30 top-10 text-xl ...">Deseja confirmar a compra?</div>
         </div>
     </div>
 </div>
