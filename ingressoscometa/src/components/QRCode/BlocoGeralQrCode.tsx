40fa38e0589a69a376aa991fd521f6184dfb9320
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2023-05-25 17:38:22 -0300

    Adicionando Bloco onde irá ficar o qrcode do ingresso
    Organizando botões share e print

--- ingressoscometa/src/components/QRCode/BlocoGeralQrCode.tsx
+++ ingressoscometa/src/components/QRCode/BlocoGeralQrCode.tsx
@@ -1,7 +1,7 @@
-import BotaoCompartilhar from "./BotaoCompartilhar";
-import BotaoImpressora from "./BotaoImpressora";
-import DivisoriaImagem from "./BlocoImagem";
-import DivisoriaDadosIngresso from "./BlocoDadosIngresso";
+import BlocoImagem from "./BlocoImagem";
+import BlocoDadosIngresso from "./BlocoDadosIngresso";
+import BlocoBotaoPrintShare from "./BlocoBotaoPrintShare";
+import BlocoQrCode from "./BlocoQRCode";
 
 export default function BlocoGeralQrCode() {
   const handleImprimir = () => {
@@ -13,16 +13,18 @@ export default function BlocoGeralQrCode() {
       className="flex items-center justify-center w-4/5 h-screen bg-white mx-auto relative"
       style={{ height: "55vh", marginTop: "-65vh", flexDirection: "row-reverse" }}
     >
-      <div className="absolute right-0 bottom-0 flex justify-between gap-5">
-        <BotaoCompartilhar />
-        <BotaoImpressora onClick={handleImprimir} />
-      </div>
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-2/3 bg-black w-px"></div>
-      <div className="absolute inset-0 z-15 left-3/4 top-40">
-        <DivisoriaDadosIngresso />
+      <div className="absolute inset-1 left-3/4 top-40">
+        <BlocoDadosIngresso />
       </div>
-      <div className="absolute inset-0 z-10 right-1/2 top-40">
-        <DivisoriaImagem />
+      <div className="absolute inset-1 left-3/4 top-40">
+        <BlocoQrCode/>
+      </div>
+      <div className="absolute inset-1 left-3/4 top-40">
+        <BlocoBotaoPrintShare />
+      </div>
+      <div className="absolute inset-0 z-15 right-1/2 top-40">
+        <BlocoImagem />
       </div>
     </div>
   );
--- ingressoscometa/src/components/QRCode/BlocoQRCode.tsx
+++ ingressoscometa/src/components/QRCode/BlocoQRCode.tsx
@@ -1,13 +1,25 @@
 export default function BlocoQrCode() {
   return (
     <div
-      className="flex items-center justify-center w-65 h-60 bg-red-500 mx-auto relative"
+      className="flex items-center justify-center w-60 h-60 bg-gray-200 mx-auto relative"
       style={{
+        right: "41.5vh",
         marginTop: "-15vh", // Metade negativa da altura para centralizar verticalmente
         color: "black", // Definir a cor do texto como preto
         fontWeight: "bold", // Definir o peso da fonte como negrito
       }}
     >
+      <span
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        QRCODE aq
+      </span>
     </div>
   );
 }
