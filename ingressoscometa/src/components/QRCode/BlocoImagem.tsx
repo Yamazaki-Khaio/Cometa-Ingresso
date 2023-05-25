cc63b3f4a73aee345f18b257dd49aca6e553bdb3
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2023-05-25 17:03:47 -0300

    Adicionando blocos na tela de QRCode

---
+++ ingressoscometa/src/components/QRCode/BlocoImagem.tsx
@@ -0,0 +1,15 @@
+export default function DivisoriaImage() {
+    return (
+      <div
+        className="flex items-center justify-center w-3/5 h-60 bg-gray-200 mx-auto relative"
+        style={{
+          marginTop: "-15vh", // Metade negativa da altura para centralizar verticalmente
+          color: "black", // Definir a cor do texto como preto
+          fontWeight: "bold", // Definir o peso da fonte como negrito
+        }}
+      >
+        <p className="text-center">Imagem aqui</p>
+      </div>
+    );
+  }
+  
---
+++ ingressoscometa/src/components/QRCode/BlocoQRCode.tsx
@@ -0,0 +1,13 @@
+export default function BlocoQrCode() {
+  return (
+    <div
+      className="flex items-center justify-center w-65 h-60 bg-red-500 mx-auto relative"
+      style={{
+        marginTop: "-15vh", // Metade negativa da altura para centralizar verticalmente
+        color: "black", // Definir a cor do texto como preto
+        fontWeight: "bold", // Definir o peso da fonte como negrito
+      }}
+    >
+    </div>
+  );
+}
