4215103c08031d6f1fb776180781cf606da193f3
lipealves13 <lipealves22@gmail.com>
2023-05-25 18:03:34 -0300

    componente de evento na visão logada

--- ingressoscometa/src/pages/carrinho.tsx
+++ ingressoscometa/src/pages/carrinho.tsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import Layout from "@/components/CabecalhoCadastro/layout"
-import TelaCarrinho  from "@/components/Carrinho/TelaCarrinho"
+import TelaCarrinho from "@/components/Carrinho/TelaCarrinho"
 import LayoutCarrinho from "@/components/Carrinho/LayoutCarrinho"
-export default function Carrinho(){
+export default function Carrinho() {
     const [cartItems, setCartItems] = useState([])
     useEffect(() => {
         fetch('/api/carrinho')
-          .then((response) => response.json())
-          .then((data) => setCartItems(data));
-      }, []);
+            .then((response) => response.json())
+            .then((data) => setCartItems(data));
+    }, []);
     return (
         <div>
-        <LayoutCarrinho pagina={<TelaCarrinho cartItems={cartItems}/>} exibirBotao={false} exibirBotao2={false} exibirBotao3={false} exibirBotao4={true} />
-        </div>  
+            <LayoutCarrinho pagina={<TelaCarrinho cartItems={cartItems} />} exibirBotao={false} exibirBotao2={false} exibirBotao3={false} exibirBotao4={true} />
+        </div>
     )
 }
