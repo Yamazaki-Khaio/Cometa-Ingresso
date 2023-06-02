8e69883def5e339f0a95a93ffa3200b1b39e1715
lipealves13 <lipealves22@gmail.com>
2023-06-02 18:14:30 -0300

    página do evento

--- ingressoscometa/src/components/ClienteLogado/EventoLogado.tsx
+++ ingressoscometa/src/components/ClienteLogado/EventoLogado.tsx
@@ -4,9 +4,9 @@ export default function EventoLogado(props: any) {
     const { imageSizeClass } = props;
     return (
 
-        <div className="relative flex flex-wrap justify-start content-center h-60 w-screen ml-12 mr-12 border bg-white  rounded-3xl">
+        <div className="relative flex flex-wrap justify-start content-center h-60 w-screen mx-12 border bg-white  rounded-3xl">
             <div className="w-92 h-44 ml-8">
-                <Image width={720} height={480} src={props.Image} alt={props.Nome} className=" w-full h-full object-cover rounded-3xl " />
+                <Image width={720} height={480} src={props.Image} alt={props.Nome} className=" w-full h-full object-fit rounded-3xl " />
             </div>
             <div className="ml-12">
                 <p className="font-bold  text-3xl">{props.Nome}</p>
--- ingressoscometa/src/pages/evento.tsx
+++ ingressoscometa/src/pages/evento.tsx
@@ -2,12 +2,13 @@
 import { useRouter } from 'next/router';
 import PaginaEvento from "@/components/Evento/PaginaEvento";
 import Layout from "@/components/CabecalhoCadastro/layout";
+import LayoutCliente from '@/components/ClienteLogado/LayoutCliente';
 
 export default function Evento() {
     const router = useRouter();
     const { id } = router.query;
   
     return (
-      <Layout pagina={<PaginaEvento id={id} />} exibirBotao={false} exibirBotao2={true} exibirBotao3={true} />
+      <LayoutCliente pagina={<PaginaEvento id={id} />} exibirBotao={false} exibirBotao2={true} exibirBotao3={true} />
     );
   }
