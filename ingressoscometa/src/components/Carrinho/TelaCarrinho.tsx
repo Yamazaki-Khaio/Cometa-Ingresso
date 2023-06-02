2b3bab5f25c68757d1be3d1137cda6ee6142896f
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2023-06-02 00:09:30 -0300

    Adicionando arquivos para implementação do envio de email ao confimar compra no carrinho

--- ingressoscometa/src/components/Carrinho/TelaCarrinho.tsx
+++ ingressoscometa/src/components/Carrinho/TelaCarrinho.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import BotaoFinalizarCompra from "./BotaoFinalizarCompra";
 
 interface Item {
     id: number;
@@ -14,6 +15,7 @@ export default function TelaCarrinho({cartItems}: CartProps) {
     return (
         <div>
             <h2 className="text-2xl font-bold">Carrinho de Compras </h2>
+            <BotaoFinalizarCompra/>
             <ul>
                 {cartItems.map((item) => ( 
                     <li key={item.id}>
