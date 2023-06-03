0708e4c36dfecd8cd79ed7118233442457350cbb
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2023-06-03 17:26:34 -0300

    Arrumando o cartão de credito usuario

--- ingressoscometa/src/components/PerfilCliente/BlocoCartaoCredito.tsx
+++ ingressoscometa/src/components/PerfilCliente/BlocoCartaoCredito.tsx
@@ -1,33 +1,58 @@
+import React, { useState } from 'react';
 import BotaoSalvarCartao from "./botaoSalvarCartao";
 import Campocvv from "./cartaoCvv";
 import CampoNumeroCartao from "./cartaoNumero";
 import CampoValidade from "./cartaoValidade";
+import Image from 'next/image';
 
 export default function BlocoCartaoDeCredito() {
+  const [hovered, setHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(false);
+  };
+
+  const getBackgroundColor = () => {
+    if (hovered) {
+      return 'linear-gradient(45deg, #1f9d78, #14b59f)';
+    }
+    return 'linear-gradient(45deg, #14b59f, #1f9d78)';
+  };
+
+  const getBoxShadow = () => {
+    if (hovered) {
+      return "10px 10px 8px rgba(0, 0, 0, 0.3)";
+    }
+    return "10px 10px 4px rgba(0, 0, 0, 0.2)";
+  };
+
   return (
     <div
-      className="flex flex-col items-center justify-center w-96 h-60 bg-teal-300 mx-auto relative"
+      className="flex flex-col items-center justify-center w-96 h-60 mx-auto relative"
       style={{
-        marginTop: "-100vh", // Metade negativa da altura para centralizar verticalmente
-        color: "black", // Definir a cor do texto como preto
+        marginTop: "-100vh",
         right: "23vh",
-        fontWeight: "bold", // Definir o peso da fonte como negrito
-        borderRadius: "30px", // Adicionar bordas levemente arredondadas
+        fontWeight: "bold",
+        borderRadius: "30px",
+        boxShadow: getBoxShadow(),
+        background: getBackgroundColor(),
+        transition: "background 0.3s ease-in-out, box-shadow 0.3s ease-in-out",
       }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
-      <CampoNumeroCartao
-        style={{
-          position: "absolute", // Posicionamento absoluto para ficar acima do bloco
-          top: "-30px", // Ajustar a distância vertical
-          fontSize: "24px", // Ajustar o tamanho da fonte
-          letterSpacing: "2px", // Aumentar o espaçamento entre as letras
-          fontWeight: "bold", // Definir o peso da fonte como negrito
-          textTransform: "uppercase", // Converter o texto em letras maiúsculas
-        }}
-      />
-      <Campocvv/>
-      <CampoValidade/>
-      <BotaoSalvarCartao/>
+            <div><CampoNumeroCartao/></div>
+      <div><Campocvv /></div>
+      <div><CampoValidade /></div>
+      <div><BotaoSalvarCartao /></div>
+      <div style={{ marginRight: "300px", marginTop: "-90px" }}>
+        <Image src="/chip.png" alt="chip" width="50" height="50" />
+      </div>
+
     </div>
   );
 }
