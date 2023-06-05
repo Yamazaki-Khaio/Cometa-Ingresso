eb434c2a22bf956cc662e4d8c3ba1a061efdb7a7
Pedro Cruz <pedrohenvcruz@gmail.com>
2023-06-05 20:06:35 -0300

    carrinho logica

--- ingressoscometa/src/components/Carrinho/TelaCarrinho.tsx
+++ ingressoscometa/src/components/Carrinho/TelaCarrinho.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import BotaoFinalizarCompra from "./BotaoFinalizarCompra";
+import Tabela from "../RelatorioEventosPromoter/TabelaEvento";
 
 interface Item {
     id: number;
@@ -10,18 +11,29 @@ interface CartProps {
     cartItems: Item[];
   }
   
-
+  const i1: Item = {
+    id: 0,
+    name: "festa latino",
+    price: 1.99
+  }
+  
   export default function TelaCarrinho({ cartItems }: CartProps) {
     return (
       <div className="flex flex-col items-center">
         <h2 className="text-2xl font-bold">Carrinho de Compras</h2>
         <ul className="mt-4">
-          {cartItems.map((item) => (
-            <li key={item.id}>
-              {item.name} - R${item.price.toFixed(2)}
+          {cartItems.map(() => (
+            <li key={i1.id}>
+              {i1.name} - R${i1.price.toFixed(2)}
             </li>
           ))}
         </ul>
+        <div className="relative flex flex-wrap justify-start content-center h-60 w-screen ml-12 mr-12 border bg-white  rounded-3xl">
+                <form>
+                <p className="font-bold  text-3xl">{i1.name}</p>
+                <p className="font-bold  text-3xl">Preço: {i1.price}</p>
+                </form>
+            </div>
         <BotaoFinalizarCompra frase="Deseja confirmar sua compra?" caminho="/qrcode"/>
       </div>
     );
