882d8b2351671a53982c6ffbbc2b01abc73ca0fa
Alisson Bomfim <alissonbomfimsilva@outlook.com>
2023-06-10 20:33:14 -0300

    Limitando acesso

--- ingressoscometa/src/pages/cadastro_evento.tsx
+++ ingressoscometa/src/pages/cadastro_evento.tsx
@@ -1,10 +1,23 @@
 import Layout from "@/components/CabecalhoCadastro/layout";
 import CadastroEvento from "@/components/CadastroEvento/Cadastro_Evento";
-
+import {useSession} from 'next-auth/react'
+import NaoUtorizado from "@/components/naoAutorizado";
 export default function cadastro(){
-    return(
-        <div>
-            <Layout pagina={<CadastroEvento/>} exibirBotao={true} exibirBotao2={true} exibirBotao3={false}/>
-        </div>
-    )
+    const { data: session } = useSession()
+
+    if(session&&session?.user.tipo==='2'){
+        return(
+            <div>
+                <Layout pagina={<CadastroEvento/>} exibirBotao={true} exibirBotao2={true} exibirBotao3={false}/>
+            </div>
+        )
+    }else{
+        
+        return(
+            <div>
+                <Layout pagina={<NaoUtorizado/>} />
+            </div>
+        )
+    }
+    
 }
