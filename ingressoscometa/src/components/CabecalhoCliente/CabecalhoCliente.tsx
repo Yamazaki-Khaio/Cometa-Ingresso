7912f684531e5782ac023337d2e8c110a91b217e
lipealves13 <lipealves22@gmail.com>
2023-06-12 09:59:02 -0300

    alterando botão logout

--- ingressoscometa/src/components/CabecalhoCliente/CabecalhoCliente.tsx
+++ ingressoscometa/src/components/CabecalhoCliente/CabecalhoCliente.tsx
@@ -1,30 +1,43 @@
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import BotaoCarrinho from "./BotaoCarrinho";
 import BotaoIngressos from "./BotaoIngressos";
 import BotaoEventos from "./BotaoEventos";
 import BotaoPerfil from "./BotaoPerfil";
 
-export default function CabecalhoCliente(props: { isBotaoOcultoLogin?: boolean,  isBotaoOcultoCadastro?: boolean, isBotaoOcultoEvento?: boolean}) {
+export default function CabecalhoCliente(props: { isBotaoOcultoLogin?: boolean, isBotaoOcultoCadastro?: boolean, isBotaoOcultoEvento?: boolean }) {
     const { data: session } = useSession()
     const route = useRouter()
-    return(
-        <div className="flex items-center justify-between p-8 w-screen h-32 bg-white">
+    return (
+        <div className="flex items-center justify-between p-8 w-screen h-40 bg-white">
             <div className="flex items-center gap-4">
                 <a href="/">
-                    <img src="/cometa2.png" alt="Logo" width="200" height="200"/>
+                    <img src="/cometa2.png" alt="Logo" width="200" height="200" />
                 </a>
-                <h2 className="font-bold text-xl">Olá {session?.user?.name}</h2>  
+                <h2 className="font-bold text-xl">Olá {session?.user?.name}</h2>
             </div>
             <div className="flex gap-6">
+            
                 <a href="/carrinho"> <BotaoCarrinho />
                 </a>
-                <a href="/clienteIngressos"><BotaoIngressos/></a>
-                
-                <BotaoEventos/>
+                <a href="/clienteIngressos"><BotaoIngressos /></a>
+
+                <BotaoEventos />
                 <a href="/perfil"> <BotaoPerfil />
+                <button className={`
+                        w-40 h-12
+                        bg-teal-900 
+                        text-white 
+                        text-24 
+                        rounded-xl`} role="button"
+                        onClick={() => { signOut({ callbackUrl: 'http://localhost:3000/login' }) }} >Sair</button>
+
+
+                    
+
+
                 </a>
-            </div>    
+            </div>
         </div>
     )
 }
--- ingressoscometa/src/components/sessao.tsx
+++ ingressoscometa/src/components/sessao.tsx
@@ -9,17 +9,7 @@ export default function Sessao() {
 
     if (session) {
         return (
-            <div>
-                <button className={`
-                        w-64 h-12
-                        bg-teal-900 
-                        text-white 
-                        text-24 
-                        rounded-xl`} role="button"
-                    onClick={() => { signOut({ callbackUrl: 'http://localhost:3000/login' }) }} >Sair</button>
-                <h1>olá {session?.user?.name}, Bem vindo</h1>
-                
-            </div>
+            console.log("Você está logado")
         )
     }
     else {
