13529b24954ecb9360cbb7fb9dc6deedc3adc32e
Alisson Bomfim <alissonbomfimsilva@outlook.com>
2023-06-12 14:58:39 -0300

    limitando acesso

--- ingressoscometa/src/pages/promotores.tsx
+++ ingressoscometa/src/pages/promotores.tsx
@@ -1,11 +1,23 @@
 import LayoutAdm from "@/components/AdmLogado/LayoutAdm"
 import TelaPromotores from "@/components/AdmLogado/TelaPromotores"
 
-export default function homeadm(){
+import { useSession } from "next-auth/react";
+import NaoUtorizado from "@/components/naoAutorizado";
 
-    return (
-        <div>
-            <LayoutAdm pagina={<TelaPromotores/>} />
-        </div>       
-    )
+export default function homeadm(){
+    const { data: session } = useSession()
+    if(session&&session.user.tipo==='3'){
+        return (
+            <div>
+                <LayoutAdm pagina={<TelaPromotores/>} />
+            </div>       
+        )
+    }else{
+        return(
+            <div>
+                <NaoUtorizado/>
+            </div>
+        )
+    }
+
 }
