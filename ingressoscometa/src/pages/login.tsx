ed9d08e6c37514f43b18b54a3275e6a891a7da90
Alana Sampaio <alana.sampaio2000@gmail.com>
2023-06-13 11:58:53 -0300

    remoção de botão de novo evento da tela de login

--- ingressoscometa/src/pages/login.tsx
+++ ingressoscometa/src/pages/login.tsx
@@ -4,7 +4,7 @@ import Login from "@/components/Login";
 export default function login(){
     return(
         <div>
-            <Layout pagina={<Login/>} exibirBotao={false} exibirBotao2={true} exibirBotao3={true} exibirBotao4={false}/>
+            <Layout pagina={<Login/>} exibirBotao={false} exibirBotao2={true} exibirBotao3={false} exibirBotao4={false}/>
         </div>
     )
 }
