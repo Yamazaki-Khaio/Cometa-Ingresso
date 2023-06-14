bb24ebbc47ebc71d6bfa4b157cdb039c497209ff
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2023-06-13 23:10:02 -0300

    Arrumando campo da data que não puxava do banco

--- ingressoscometa/src/components/PromoterLogado/EditarEvento/CampoDataEvento.tsx
+++ ingressoscometa/src/components/PromoterLogado/EditarEvento/CampoDataEvento.tsx
@@ -12,41 +12,40 @@ export default function CampoDataEvento(props: any) {
 
   useEffect(() => {
     if (!editando) {
-      setDataEvento(props.dataEvento); // Atualiza o valor do nome apenas se não estiver editando
+      setDataEvento(props.data); // Atualiza o valor da data apenas se não estiver editando
     }
-  }, [props.dataEvento, editando]);
+  }, [props.data, editando]);
 
-  function handleDataEventoChange(event: React.ChangeEvent<HTMLInputElement>){//Modifica o valor do nome do evento
+  function handleDataEventoChange(event: React.ChangeEvent<HTMLInputElement>) {
     setDataEvento(event.target.value);
   }
+
   return (
     <>
-    <label htmlFor="data">
-      Data do evento:
-      {props.optional ? (
+      <label htmlFor="data">
+        Data do evento:
+        {props.optional ? (
           <span className="text-red-600 text-bold">*</span>
         ) : null}
-    </label>
-    <div className="flex">
-    <input
-      value={dataEvento}
-      type="date"
-      name="data"
-      id="data"
-      placeholder="  /  /  "
-      disabled={!editando}
-      required
-      className="border w-36 border-gray-400 rounded-md p-2 mb-8"
-      onChange={handleDataEventoChange}
-    />
-    <span className="input-group-btn p-2">
-        <button type="button" className="btn btn-default" onClick={handleEditar}>
-          <FontAwesomeIcon icon={faPenToSquare} size="lg" />
-        </button>
-    </span>
-  </div>
+      </label>
+      <div className="flex">
+        <input
+          value={dataEvento}
+          type="date"
+          name="data"
+          id="data"
+          placeholder="  /  /  "
+          disabled={!editando}
+          required
+          className="border w-36 border-gray-400 rounded-md p-2 mb-8"
+          onChange={handleDataEventoChange}
+        />
+        <span className="input-group-btn p-2">
+          <button type="button" className="btn btn-default" onClick={handleEditar}>
+            <FontAwesomeIcon icon={faPenToSquare} size="lg" />
+          </button>
+        </span>
+      </div>
     </>
-    
-  
   );
 }
